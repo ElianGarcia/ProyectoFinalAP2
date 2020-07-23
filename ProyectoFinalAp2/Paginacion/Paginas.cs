704664e23293e6f35861eddd54133a48f1170caa
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-07-23 14:07:00 -0400

    Paginacion de categorias creada...

---
+++ ProyectoFinalAp2/Paginacion/Paginas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp2.Paginacion
+{
+    public class Paginas
+    {
+        public int pagina { get; set; } = 1;
+        public int mostrarCantidad { get; set; } = 4;
+    }
+}
