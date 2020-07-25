e2cf11a6be340089a83ceac5d0039449a0ea821f
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-07-25 13:18:18 -0400

    Agregando funcionalidad para imprimir desde factura..

---
+++ ProyectoFinalAp2/Data/AuxFacturaFinal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAp2.Data
+{
+    public class AuxFacturaFinal
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Importe { get; set; }
+
+        public AuxFacturaFinal()
+        {
+            ID = 0;
+            Descripcion = string.Empty;
+            Cantidad = 0;
+            Precio = 0;
+            Importe = 0;
+        }
+    }
+}
